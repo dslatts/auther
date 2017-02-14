1b420bedaa570c9383f5ec4c30884eca198163d6
Drew Slattery <drewslatts@gmail.com>
2017-02-14 15:14:44 -0500

    3. sessions

--- server/app/index.js
+++ server/app/index.js
@@ -2,6 +2,9 @@
 
 var app = require('express')();
 var path = require('path');
+const session = require('express-session');
+const User = require('../api/users/user.model.js');
+var Sequelize = require('sequelize');
 
 // "Enhancing" middleware (does not send response, server-side effects only)
 
@@ -9,8 +12,36 @@ app.use(require('./logging.middleware'));
 
 app.use(require('./body-parsing.middleware'));
 
+//session setup
+
+app.use(session({
+  secret: 'hoopla',
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(function (req, res, next) {
+  console.log('session', req.session);
+  next();
+});
 
 // "Responding" middleware (may send a response back to client)
+
+app.post('/login', function(req, res, next) {
+  User.findOne({
+    where: req.body
+  })
+  .then((user) => {
+    if (!user) {
+      res.sendStatus(401);
+    }
+    else {
+      req.session.userId = user.id;
+      res.sendStatus(200);
+    }
+  })
+  .catch(next);
+});
 
 app.use('/api', require('../api/api.router'));
 
